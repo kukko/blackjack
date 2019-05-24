e5dd02f48daa08bbd0b3cba2d77ee77c17b4c49f
Nyárai Gergő <nyariag@gmail.com>
2019-05-24 21:01:15 +0200

    --enhancement: FRONT_END: Create Angular app for FrontEnd.

--- controllers/FrontEndController/FrontEndController.js
+++ controllers/FrontEndController/FrontEndController.js
@@ -2,7 +2,15 @@ let { BaseController } = require("web-wombat");
 
 class FrontEndController extends BaseController{
 	serve(){
-		this.view("index");
+		let route = this.request.routeVariables.route,
+			{ readFileSync } = require("fs"),
+			{ resolve } = require("path");
+		if (route.length > 0){
+			this.response.end(readFileSync(resolve(require.main.filename, "../client/dist/", route)));
+		}
+		else{
+			this.response.end(readFileSync(resolve(require.main.filename, "../client/dist/index.html")));
+		}
 	}
 }
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 let { Route, MiddlewareProvider } = require("web-wombat");
 
 module.exports = [
-	Route.get("/", require("../controllers/FrontEndController/FrontEndController")),
+	Route.get("/{route}", require("../controllers/FrontEndController/FrontEndController")),
 	Route.websocket("/game", require("../controllers/GameController/GameController"))
 ];
